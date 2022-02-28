5b41b4067498b51223325503b01f38ea48f23a2b
Mahi787 <ishatnoormahi787@gmail.com>
2022-02-28 19:34:02 +0600

    modal and more

---
+++ resources/js/raffle.js
@@ -0,0 +1,16 @@
+
+// TOUR MODAL
+let disabledModalBtn = document.getElementById('take_tour_btn');
+
+document.body.onload = function(){
+    setTimeout(()=>{
+        disabledModalBtn.click();
+    },2000)
+}
+
+//tutorial demo
+
+//import demo names
+
+//main draw
+
